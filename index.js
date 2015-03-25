060710b61bcd74255974cb4dd20c45563ba7a9bd
fakeyou <ag.nanninga@gmail.com>
2015-03-25 22:54:19 +0100

    added metalsmith plugins: metallic, assets, collections

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 'use strict';
 
-var Metalsmith = require('metalsmith');
-var serve      = require('metalsmith-serve');
-var watch      = require('metalsmith-watch');
-var markdown   = require('metalsmith-markdown');
-var layouts    = require('metalsmith-layouts');
+var Metalsmith  = require('metalsmith');
+var serve       = require('metalsmith-serve');
+var watch       = require('metalsmith-watch');
+var markdown    = require('metalsmith-markdown');
+var layouts     = require('metalsmith-layouts');
+var assets      = require('metalsmith-assets');
+var collections = require('metalsmith-collections');
+var metallic    = require('metalsmith-metallic');
 
 // todo: load environment specific config
 var config = require('./config/dev.json');
@@ -14,8 +17,11 @@ Metalsmith(__dirname)
   .destination(config.destination)
   .clean(config.clean)
 
+  .use(metallic(config.metallic))
   .use(markdown(config.markdown))
+  .use(collections(config.collections))
   .use(layouts(config.layouts))
+  .use(assets(config.assets))
 
   .use(watch(config.watch))
   .use(serve(config.serve))
