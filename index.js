30c79e33a98d28fcad1c81df9b7eed7d2866898f
unknown <ag.nanninga@gmail.com>
2015-07-10 18:42:39 +0200

    moved some folders and fixed watch/serve flow

--- index.js
+++ index.js
@@ -6,8 +6,11 @@ var watch       = require('metalsmith-watch');
 var markdown    = require('metalsmith-markdown');
 var layouts     = require('metalsmith-layouts');
 var assets      = require('metalsmith-assets');
+var ignore      = require('metalsmith-ignore');
 var collections = require('metalsmith-collections');
 var metallic    = require('metalsmith-metallic');
+var moveup      = require('metalsmith-move-up');
+var define      = require('metalsmith-define');
 
 // todo: load environment specific config
 var config = require('./config/dev.json');
@@ -17,11 +20,15 @@ Metalsmith(__dirname)
   .destination(config.destination)
   .clean(config.clean)
 
+  .use(define(config.define))
+
   .use(metallic(config.metallic))
   .use(markdown(config.markdown))
   .use(collections(config.collections))
   .use(layouts(config.layouts))
-  .use(assets(config.assets))
+  .use(ignore(config.ignore))
+  // .use(assets(config.assets))
+  .use(moveup(config.moveup))
 
   .use(watch(config.watch))
   .use(serve(config.serve))
