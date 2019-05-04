3c2acc1b46db8efbcf4d37d914c492b0b7019d0a
André Nanninga <andre@nannin.ga>
2019-05-04 20:01:05 +0200

    fix: typescript config

--- next.config.js
+++ next.config.js
@@ -1,28 +1,7 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 
-function getServerPhase() {
-  if (process.env.NODE_ENV === 'development') {
-    return null
-  }
+const withTypescript = require('@zeit/next-typescript')
 
-  if (process.env.NOW_REGION) {
-    return require('next/constants').PHASE_PRODUCTION_SERVER
-  }
-
-  return require('next-server/constants').PHASE_PRODUCTION_SERVER
-}
-
-module.exports = phase => {
-  if (phase === getServerPhase()) {
-    return {}
-  }
-
-  const withTypescript = require('@zeit/next-typescript')
-  const withMDX = require('@zeit/next-mdx')()
-
-  return withMDX(
-    withTypescript({
-      target: 'serverless',
-    })
-  )
-}
+module.exports = withTypescript({
+  target: 'serverless',
+})
