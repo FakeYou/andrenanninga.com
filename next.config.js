3e6f6264e64735a31f627e575f3cc53618b9dc1b
André Nanninga <andre@nannin.ga>
2019-05-04 20:08:23 +0200

    fix: next config for production

--- next.config.js
+++ next.config.js
@@ -1,7 +1,25 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 
-const withTypescript = require('@zeit/next-typescript')
+function getServerPhase() {
+  if (process.env.NODE_ENV === 'development') {
+    return null
+  }
 
-module.exports = withTypescript({
-  target: 'serverless',
-})
+  if (process.env.NOW_REGION) {
+    return require('next/constants').PHASE_PRODUCTION_SERVER
+  }
+
+  return require('next-server/constants').PHASE_PRODUCTION_SERVER
+}
+
+module.exports = phase => {
+  if (phase === getServerPhase()) {
+    return {}
+  }
+
+  const withTypescript = require('@zeit/next-typescript')
+
+  return withTypescript({
+    target: 'serverless',
+  })
+}
